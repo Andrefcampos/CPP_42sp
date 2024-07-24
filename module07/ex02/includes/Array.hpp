5ce9236a8b5859ef7b2301b3ea24a1c92259f490
Andrefcampos <andrecampos.wnr@gmail.com>
2024-07-24 01:42:56 -0300

    Update: file Array.hpp

--- module07/ex02/includes/Array.hpp
+++ module07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 05:57:50 by andrefil          #+#    #+#             */
-/*   Updated: 2024/07/24 01:19:30 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/07/24 01:33:58 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define ARRAY_HPP
 
 #include <cstddef>
+#include <ostream>
+
 template <typename T>
 class	Array {
 
@@ -29,6 +31,11 @@ public:
 
 	Array	&operator=(Array const &param);
 
+	const T	&operator[](unsigned int const &index) const;
+	T	&operator[](unsigned int const &index);
 };
+
+template <typename T>
+std::ostream	&operator<<(std::ostream &oS, Array<T> const &param);
 
 #endif
