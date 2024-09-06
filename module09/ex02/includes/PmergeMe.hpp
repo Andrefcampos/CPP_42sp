88c13cf3b71f2235f850f510214f3fcd23757cc9
Andrefcampos <andrecampos.wnr@gmail.com>
2024-09-06 06:46:11 -0300

    Upload: Makefile ex02

--- module09/ex02/includes/PmergeMe.hpp
+++ module09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,19 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 04:21:29 by andrefil          #+#    #+#             */
-/*   Updated: 2024/09/06 04:21:30 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/09/06 06:37:14 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGE_ME_HPP
+# define PMERGE_ME_HPP
+
+class	PmergeMe {
+
+private:
+
+public:
+
+};
+
+#endif
